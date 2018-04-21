a237990aa58f7554efc0b1d70b1e98ea40353b36
Nick Coghlan <ncoghlan@gmail.com>
2018-04-21 16:41:52 +1000

    Make Mercurial reference technology neutral

--- pep0/constants.py
+++ pep0/constants.py
@@ -24,7 +24,7 @@ Created: 13-Jul-2000
 intro = u"""
     This PEP contains the index of all Python Enhancement Proposals,
     known as PEPs.  PEP numbers are assigned by the PEP editors, and
-    once assigned are never changed[1].  The Mercurial history[2] of
+    once assigned are never changed[1].  The version control history[2] of
     the PEP texts represent their historical record.
 """
 
