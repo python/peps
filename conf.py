eb795e00082dbce2e19c7f0a7bcd7605d8f2926c
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-04-25 23:43:59 +0100

    Update conf

--- conf.py
+++ conf.py
@@ -2,30 +2,24 @@
 
 # -- Path setup --------------------------------------------------------------
 
-import docutils
-from docutils.readers import standalone
-from docutils.transforms import frontmatter, references, misc
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.append(os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'peps'
+project = 'PEPs'
 copyright = '2020, AUTHNAME'
 author = 'AUTHNAME'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
+version = "1.x"
 
 html_title = "Python Enhancement Proposals (PEPs)"
-html_short_title = "PEPs Home" #
+html_short_title = "PEPs Home"
 html_show_copyright = False
 html_show_sphinx = False
 
@@ -56,7 +50,8 @@ exclude_patterns = [
     'venv',
     'build',
     'README.rst',
-    'CONTRIBUTING.rst'
+    'CONTRIBUTING.rst',
+    'requirements.txt',
 ]
 
 # The name of the Pygments (syntax highlighting) style to use.
@@ -74,30 +69,4 @@ html_theme = 'classic'
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
 
-
-def new_get_transforms(self):
-    # Replicate result of standalone.Reader.get_transforms()
-    transforms = docutils.readers.Reader.get_transforms(self) + [
-        references.Substitutions,
-        references.PropagateTargets,
-        frontmatter.DocTitle,
-        frontmatter.SectionSubTitle,
-        frontmatter.DocInfo,
-        references.AnonymousHyperlinks,
-        references.IndirectHyperlinks,
-        references.Footnotes,
-        references.ExternalTargets,
-        references.InternalTargets,
-        references.DanglingReferences,
-        misc.Transitions,
-    ]
-
-    # Explicitly remove these transforms, as we implement PEP-specific pre-processing
-    # transforms.remove(frontmatter.DocTitle)
-    # transforms.remove(frontmatter.SectionSubTitle)
-    transforms.remove(frontmatter.DocInfo)
-
-    return transforms
-
-
-standalone.Reader.get_transforms = new_get_transforms
+master_doc = 'contents'
