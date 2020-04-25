27fde33b344c175949fe77deed553f1b5036f6fa
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-04-25 17:19:47 +0100

    Fix title generation
    Also add pep_parser as I missed it

--- pepreader/__init__.py
+++ pepreader/__init__.py
@@ -6,9 +6,8 @@ __version__ = '1.0.0'
 
 def setup(app: Sphinx):
     """Initialize Sphinx extension."""
-    from .pep_parser import PEPParser, PEPTitleCollector
+    from .pep_parser import PEPParser
 
-    # app.add_env_collector(PEPTitleCollector)
     app.add_source_parser(PEPParser)
 
     return {'version': __version__, 'parallel_read_safe': True}
