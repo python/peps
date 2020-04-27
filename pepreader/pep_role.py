ab28391cd47a68011f26449d3668eda324a30fb2
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-04-27 19:08:08 +0100

    fix import error

--- pepreader/pep_role.py
+++ pepreader/pep_role.py
@@ -1,8 +1,40 @@
-from sphinx import roles
+from typing import List, Tuple
+
+from docutils import nodes
+from docutils.nodes import Node, system_message
+
+from sphinx import addnodes
+from sphinx.locale import _
+from sphinx.util.docutils import ReferenceRole
+
 import pepreader
 
 
-class PEPRole(roles.PEP):
+class PEPRole(ReferenceRole):
+    def run(self) -> Tuple[List[Node], List[system_message]]:
+        target_id = 'index-%s' % self.env.new_serialno('index')
+        entries = [('single', _('Python Enhancement Proposals; PEP %s') % self.target,
+                    target_id, '', None)]
+
+        index = addnodes.index(entries=entries)
+        target = nodes.target('', '', ids=[target_id])
+        self.inliner.document.note_explicit_target(target)
+
+        try:
+            refuri = self.build_uri()
+            reference = nodes.reference('', '', internal=False, refuri=refuri, classes=['pep'])
+            if self.has_explicit_title:
+                reference += nodes.strong(self.title, self.title)
+            else:
+                title = "PEP " + self.title
+                reference += nodes.strong(title, title)
+        except ValueError:
+            msg = self.inliner.reporter.error('invalid PEP number %s' % self.target,
+                                              line=self.lineno)
+            prb = self.inliner.problematic(self.rawtext, self.rawtext, msg)
+            return [prb], [msg]
+
+        return [index, target, reference], []
 
     def build_uri(self) -> str:
         base_url = self.inliner.document.settings.pep_base_url
