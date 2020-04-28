29567678a3ab4935557ff4162365e2990416304a
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-04-28 18:07:22 +0100

    Update build to include 3.8 and sanity checks

--- build.py
+++ build.py
@@ -1,15 +1,48 @@
 # Build script for Sphinx documentation
 
+import argparse
 from pathlib import Path
 from sphinx.application import Sphinx
 
+
+def create_parser():
+
+    parser = argparse.ArgumentParser(description="Build PEP documents")
+    arguments = [
+        ('-b', '--builder', 'store'),
+        ('-d', '--dir-html', 'store_true'),
+        ('-c', '--check-links', 'store_true'),
+        ('-f', '--fail-on-warning', 'store_true'),
+        ('-n', '--nitpicky', 'store_true'),
+    ]
+    for arg in arguments:
+        parser.add_argument(arg[0], arg[1], action=arg[2])
+
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
+    args = create_parser()
+
     root_directory = Path('.').absolute()
     source_directory = root_directory
     configuration_directory = source_directory
     build_directory = root_directory / 'build'
     doctree_directory = build_directory / '.doctrees'
-    builder = 'html'
 
-    app = Sphinx(source_directory, configuration_directory, build_directory, doctree_directory, builder)
+    if args.check_links:
+        builder = 'linkcheck'
+    elif args.dir_html:
+        builder = 'dirhtml'
+    else:
+        builder = 'html'
+
+    config_overrides = {}
+    if args.nitpicky:
+        config_overrides['nitpicky'] = True
+
+    app = Sphinx(
+        source_directory, configuration_directory, build_directory, doctree_directory, builder,
+        confoverrides=config_overrides, warningiserror=args.fail_on_warning, keep_going=args.fail_on_warning,
+    )
     app.build()
