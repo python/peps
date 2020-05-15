2ddd470a70e6693dcf4f2795ec964cac45cdad81
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-05-15 22:20:44 +0100

    Improve packaging process
    (for transforms to pydotorg standards)

--- conf.py
+++ conf.py
@@ -60,7 +60,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
+
 html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
--- package.py
+++ package.py
@@ -1,7 +1,7 @@
 """Transforms Sphinx HTML output into  python.org input format"""
 
-from bs4 import BeautifulSoup
 from pathlib import Path
+from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
     root_path = Path(".")
@@ -12,29 +12,102 @@ if __name__ == '__main__':
     for file_path in html_path.glob("pep-*"):
         if file_path.suffix not in '.html':
             continue
+        print(file_path.stem)
 
         soup = BeautifulSoup(file_path.read_text(encoding="UTF8"), 'lxml')
         contents = soup.find('div', class_="body").div
+
+        # Removes <p> tags from list item elements
+        for tag in contents.findAll("li"):
+            try:
+                tag.p.unwrap()
+            except AttributeError:
+                # If no <p> tag to unwrap
+                pass
+
+        # Removes all permalink elements
+        [tag.decompose() for tag in contents.findAll(class_="headerlink")]
+
+        # Replace brackets class with [ and ]
+        for tag in contents.findAll("a", class_="brackets"):
+            tag.insert(0, "[")
+            tag.append("]")
+            tag["class"].remove("brackets")
+
+        # Remove Sphinx Header
+        contents.h1.decompose()
+
+        # Promotes all remaining headers
+        for level in range(6 - 1):
+            h_level = level + 2
+            headers = contents.findAll(f"h{h_level}")
+            for header in headers:
+                header.name = f"h{h_level - 1}"
+
         dl = contents.find('dl')
+
+        # Adds horizontal rule
+        dl.insert_after(soup.new_tag("hr"))
+
+        # Parses the PEP Info box to transform to pydotorg standards
         for tag in dl.findChildren():
             if tag.name == "dt":
                 tag.name = "th"
-                tag.find_next_sibling().name = "td"
+                tag.string += ":"
                 tag.attrs['class'] = 'field-name'
-                tag.find_next_sibling()['class'] = 'field-body'
+                value_tag = tag.find_next_sibling()
+                value_tag.name = "td"
+                value_tag.string = value_tag.text.strip("\n")
+                value_tag['class'] = 'field-body'
+
+                # Wrap the key-value pair in a <tr> element
                 tr = soup.new_tag("tr", **{'class': 'field'})
                 tag.insert_before(tr)
-                tr.insert(0, tag.find_next_sibling())
+                tr.insert(0, value_tag)
                 tr.insert(0, tag)
 
         dl.name = 'tbody'
-        tbl = soup.new_tag('table', **{'class': dl['class']})
+
+        classes = dl['class']
+        classes.remove("simple")
+        classes.append("docutils")
+        del dl['class']
+        tbl = soup.new_tag('table', **{'class': classes})
         dl.wrap(tbl)
-        dl['class'] = []
         tbl.insert(0, soup.new_tag("col", **{'class': "field-body"}))
         tbl.insert(0, soup.new_tag("col", **{'class': "field-name"}))
 
-        write_path = Path('./package/peps') / file_path.name
-        write_path.write_text(str(contents), encoding="UTF8")
+        # Fix footnotes/references
+        dl_refs = contents.findAll('dl', class_="footnote brackets")
 
-        del soup, contents, dl, tbl
+        for ref in dl_refs:
+            footnote_rows = []
+            for tag in ref.findChildren():
+                if tag.name == "dt":
+                    tag.name = "td"
+                    tag.attrs['class'] = 'label'
+                    if tag.span and "brackets" in tag.span.get("class"):
+                        tag.span.insert(0, "[")
+                        tag.span.append("]")
+                        tag.span.unwrap()
+
+                    value_tag = tag.find_next_sibling()
+                    value_tag.name = "td"
+                    value_tag.string = value_tag.text.strip("\n")
+
+                    # Wrap the key-value pair in a <tr> element
+                    tr = soup.new_tag("tr")
+                    tr.insert(0, value_tag)
+                    tr.insert(0, tag)
+                    footnote_rows.append(tr)
+            ref.name = 'table'
+            ref["class"] = "docutils footnote"
+            ref.contents = footnote_rows
+            # TODO combine all tables into one (relianbt on fixingf PEP8 table mismatch)
+
+        # Writes transformed HTML
+        write_path = Path('./package/peps') / file_path.name
+        html = [str(i) for i in contents.contents]
+        write_path.write_text(str("".join(html)), encoding="UTF8")
+
+        del soup, contents, headers, dl, tbl, dl_refs, html
