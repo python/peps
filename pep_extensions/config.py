c9da1534312073ef5873bbcc9b8ab0f1741004e0
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-06-21 19:42:25 +0100

    Remove header fields as per pydotorg converters

--- pep_extensions/config.py
+++ pep_extensions/config.py
@@ -1,8 +1,6 @@
 """Misc. config variables for the PEP extensions module."""
-import re
 
 __version__ = '1.0.0'
 pep_stem = "pep-{:0>4}"
 pep_url = f"{pep_stem}.html"
-pep_vcs_url = f"https://github.com/python/peps/blob/master/{pep_stem}.txt"
-rcs_keyword_substitutions = [(re.compile(r"\$[a-zA-Z]+: (.+) \$$"), r"\1")]
+pep_vcs_url = "https://github.com/python/peps/blob/master/{}"
