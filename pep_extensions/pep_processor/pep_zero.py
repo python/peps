3ffd0d00ed852564009f158ac215d3e6ed07a828
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-06-21 19:33:31 +0100

    Type hinting: pep_zero.py

--- pep_extensions/pep_processor/pep_zero.py
+++ pep_extensions/pep_processor/pep_zero.py
@@ -26,41 +26,40 @@ class PEPZeroSpecial(nodes.SparseNodeVisitor):
     Perform the special processing needed by PEP 0:
 
     - Mask email addresses.
-
     - Link PEP numbers in the second column of 4-column tables to the PEPs
       themselves.
     """
 
-    def __init__(self, document):
+    def __init__(self, document: nodes.document):
         super().__init__(document)
-        self.pep_table = None
-        self.entry = None
+        self.pep_table: int = 0
+        self.entry: int = 0
 
-    def unknown_visit(self, node):
+    def unknown_visit(self, node: nodes.Node) -> None:
         pass
 
     @staticmethod
-    def visit_reference(node):
+    def visit_reference(node: nodes.reference) -> None:
         node.replace_self(peps.mask_email(node))
 
     @staticmethod
-    def visit_field_list(node):
+    def visit_field_list(node: nodes.field_list) -> None:
         if 'rfc2822' in node['classes']:
             raise nodes.SkipNode
 
-    def visit_tgroup(self, node):
+    def visit_tgroup(self, node: nodes.tgroup) -> None:
         self.pep_table = node['cols'] == 4
         self.entry = 0
 
-    def visit_colspec(self, node):
+    def visit_colspec(self, node: nodes.colspec) -> None:
         self.entry += 1
         if self.pep_table and self.entry == 2:
             node['classes'].append('num')
 
-    def visit_row(self, node):
+    def visit_row(self, _node: nodes.row) -> None:
         self.entry = 0
 
-    def visit_entry(self, node):
+    def visit_entry(self, node: nodes.entry) -> None:
         self.entry += 1
         if self.pep_table and self.entry == 2 and len(node) == 1:
             node['classes'].append('num')
@@ -69,8 +68,7 @@ class PEPZeroSpecial(nodes.SparseNodeVisitor):
                 text = p.astext()
                 try:
                     pep = int(text)
-                    ref = (self.document.settings.pep_base_url
-                           + pep_url.format(pep))
+                    ref = self.document.settings.pep_base_url + pep_url.format(pep)
                     p[0] = nodes.reference(text, text, refuri=ref)
                 except ValueError:
                     pass
