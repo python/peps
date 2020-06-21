d5aab3bb02d0a5e3fb1160aa2edc533489bf5834
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-06-21 20:00:50 +0100

    Add math2html maths processor

--- conf.py
+++ conf.py
@@ -64,7 +64,7 @@ pygments_style = 'sphinx'
 
 html_theme_path = ["pep_extensions"]
 html_theme = "theme"
-
+html_math_renderer = "math2html"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
--- pep_extensions/__init__.py
+++ pep_extensions/__init__.py
@@ -1,12 +1,11 @@
 """Sphinx extensions for performant PEP processing"""
 from sphinx.application import Sphinx
+from docutils.writers.html5_polyglot import HTMLTranslator
 
 from pep_extensions.config import __version__
-
+from pep_extensions.pep_processor import pep_html_translator
 from pep_extensions.pep_processor import pep_parser
 from pep_extensions.pep_processor import pep_role
-from pep_extensions.pep_processor import pep_html_translator
-
 from pep_extensions.pepzero.generate_pep_index import create_pep_zero
 
 
@@ -17,5 +16,6 @@ def setup(app: Sphinx):
     app.add_role('pep', pep_role.PEPRole(), override=True)
     app.set_translator("html", pep_html_translator.PEPTranslator)
     app.connect("env-before-read-docs", create_pep_zero)
+    app.add_html_math_renderer('math2html', (HTMLTranslator.visit_math, None), (HTMLTranslator.visit_math_block, None))  # NoQA
 
     return {'version': __version__, 'parallel_read_safe': True, 'parallel_write_safe': True}
