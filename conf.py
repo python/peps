126195ab3b219fa6a29974a663d74c1a3d65f6d3
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-06-22 04:52:14 +0100

    Clean conf.py

--- conf.py
+++ conf.py
@@ -2,11 +2,9 @@
 
 # -- Path setup --------------------------------------------------------------
 
-
 import sys
 from pathlib import Path
 sys.path.extend(str(Path('./pep_extensions').absolute()))
-
 
 # -- Project information -----------------------------------------------------
 
@@ -14,19 +12,11 @@ project = 'PEPs'
 copyright = '2020, PEP Authors'
 author = 'PEP Authors'
 
-# The full version, including alpha/beta/rc tags
-# release = '1.0.0'
-
-html_title = "PEPs.Python.org"
-html_show_copyright = False
-html_show_sphinx = False
-template_bridge = "pep_extensions.pep_processor.html.pep_jinja2.PEPTemplateLoader"
+master_doc = 'contents'
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# Add any Sphinx extension module names here, as strings.
 extensions = ["pep_extensions", "sphinx.ext.githubpages"]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -39,9 +29,8 @@ source_suffix = {
     '.txt': 'pep',
 }
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+# List of patterns, relative to source directory, to ignore when
+# looking for source files.
 exclude_patterns = [
     '_build',
     'Thumbs.db',
@@ -59,16 +48,14 @@ exclude_patterns = [
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-
-html_theme_path = ["pep_extensions"]
-html_theme = "theme"
-html_favicon = Path(html_theme_path[0], html_theme, "static/py.png").as_posix()
+# HTML output settings
 html_math_renderer = "math2html"
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_show_copyright = False
+html_show_sphinx = False
+html_title = "PEPs.Python.org"
 
-master_doc = 'contents'
+# Theme settings
+html_theme = "theme"
+html_theme_path = ["pep_extensions"]
+html_favicon = Path(html_theme_path[0], html_theme, "static/py.png").as_posix()
+template_bridge = "pep_extensions.pep_processor.html.pep_jinja2.PEPTemplateLoader"
