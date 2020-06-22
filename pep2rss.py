754b684028fc2b2346965974ec2daa6032558089
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-06-22 04:02:09 +0100

    Clean up RSS generation

--- pep2rss.py
+++ pep2rss.py
@@ -1,74 +1,77 @@
-#!/usr/bin/env python3
-
-# usage: pep-hook.py $REPOS $REV
+# usage: pep2rss.py $REPOS $REV
 # (standard post-commit args)
 
-import os, glob, time, datetime, stat, re, sys
-import PyRSS2Gen as rssgen
+import datetime
+import re
+from pathlib import Path
 
-RSS_PATH = os.path.join(sys.argv[1], 'peps.rss')
+from feedgen import entry
+from feedgen import feed
+
 
 def firstline_startingwith(full_path, text):
-    for line in open(full_path, encoding="utf-8"):
+    for line in full_path.open(encoding="utf-8"):
         if line.startswith(text):
             return line[len(text):].strip()
     return None
 
-# get list of peps with creation time
-# (from "Created:" string in pep .rst or .txt)
-peps = glob.glob('pep-*.txt')
-peps.extend(glob.glob('pep-*.rst'))
+
 def pep_creation_dt(full_path):
-    created_str = firstline_startingwith(full_path, 'Created:')
-    # bleh, I was hoping to avoid re but some PEPs editorialize
-    # on the Created line
-    m = re.search(r'''(\d+-\w+-\d{4})''', created_str)
+    created_str = firstline_startingwith(full_path, "Created:")
+    # bleh, I was hoping to avoid re but some PEPs editorialize on the Created line
+    m = re.search(r"(\d+-\w+-\d{4})", created_str)
     if not m:
-        # some older ones have an empty line, that's okay, if it's old
-        # we ipso facto don't care about it.
-        # "return None" would make the most sense but datetime objects
-        # refuse to compare with that. :-|
-        return datetime.datetime(*time.localtime(0)[:6])
+        # some older ones have an empty line, that's okay, if it's old we ipso facto don't care about it.
+        # "return None" would make the most sense but datetime objects refuse to compare with that. :-|
+        return datetime.datetime.fromtimestamp(0, tz=datetime.timezone.utc)
     created_str = m.group(1)
     try:
-        t = time.strptime(created_str, '%d-%b-%Y')
+        dt = datetime.datetime.strptime(created_str, "%d-%b-%Y")
     except ValueError:
-        t = time.strptime(created_str, '%d-%B-%Y')
-    return datetime.datetime(*t[:6])
-peps_with_dt = [(pep_creation_dt(full_path), full_path) for full_path in peps]
-# sort peps by date, newest first
-peps_with_dt.sort(reverse=True)
+        dt = datetime.datetime.strptime(created_str, "%d-%B-%Y")
+    return dt.replace(tzinfo=datetime.timezone.utc)
 
-# generate rss items for 10 most recent peps
-items = []
-for dt, full_path in peps_with_dt[:10]:
-    try:
-        n = int(full_path.split('-')[-1].split('.')[0])
-    except ValueError:
-        pass
-    title = firstline_startingwith(full_path, 'Title:')
-    author = firstline_startingwith(full_path, 'Author:')
-    url = 'http://www.python.org/dev/peps/pep-%0.4d' % n
-    item = rssgen.RSSItem(
-        title = 'PEP %d: %s' % (n, title),
-        link = url,
-        description = 'Author: %s' % author,
-        guid = rssgen.Guid(url),
-        pubDate = dt)
-    items.append(item)
 
-# the rss envelope
-desc = """
-Newest Python Enhancement Proposals (PEPs) - Information on new
-language features, and some meta-information like release
-procedure and schedules
-""".strip()
-rss = rssgen.RSS2(
-    title = 'Newest Python PEPs',
-    link = 'http://www.python.org/dev/peps',
-    description = desc,
-    lastBuildDate = datetime.datetime.now(),
-    items = items)
+def main():
+    # get list of peps with creation time (from "Created:" string in pep source)
+    peps_with_dt = [(pep_creation_dt(path), path) for path in Path().glob("pep-????.*")]
+    peps_with_dt.sort(reverse=True)  # sort peps by date, newest first
 
-with open(RSS_PATH, 'w', encoding="utf-8") as fp:
-    fp.write(rss.to_xml(encoding="utf-8"))
+    # generate rss items for 10 most recent peps
+    items = []
+    for dt, full_path in peps_with_dt[:10]:
+        try:
+            n = int(full_path.stem.split("-")[-1])
+        except ValueError:
+            continue
+        title = firstline_startingwith(full_path, "Title:")
+        author = firstline_startingwith(full_path, "Author:")
+        url = f"http://www.python.org/dev/peps/pep-{n:0>4}"
+        item = entry.FeedEntry()
+        item.title(f"PEP {n}: {title}")
+        item.link(href=url)
+        item.description(f"Author: {author}")
+        item.guid(url, permalink=True)
+        item.pubDate(dt)
+        items.append(item)
+
+    # the rss envelope
+    desc = """
+    Newest Python Enhancement Proposals (PEPs) - Information on new
+    language features, and some meta-information like release
+    procedure and schedules
+    """.strip()
+    fg = feed.FeedGenerator()
+    fg.language('en')
+    fg.title('Newest Python PEPs')
+    fg.link(href='http://www.python.org/dev/peps')
+    fg.description(desc)
+    fg.lastBuildDate(datetime.datetime.now(tz=datetime.timezone.utc))
+    for item in reversed(items):
+        fg.add_entry(item)
+
+    Path("peps.rss").write_bytes(fg.rss_str())
+
+
+if __name__ == "__main__":
+    main()
