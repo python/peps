5240d4b8856791a6d7352b68b649fd166d9ea792
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2020-08-15 06:42:54 +0100

    Add last-modified date to footer re python/pythondotorg#1564

--- pep_extensions/config.py
+++ pep_extensions/config.py
@@ -4,3 +4,4 @@ __version__ = '1.0.0'
 pep_stem = "pep-{:0>4}"
 pep_url = f"{pep_stem}.html"
 pep_vcs_url = "https://github.com/python/peps/blob/master/{}"
+pep_commits_url = "https://github.com/python/peps/commits/master/{}"
