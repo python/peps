3906ee0027dc6aac104b4a6b15a5aef0490a87ea
Hugo van Kemenade <hugovk@users.noreply.github.com>
2020-10-23 14:02:51 +0300

    Use first paragraph of abstract as 'description', and PEP author as 'author'

--- pep2rss.py
+++ pep2rss.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-# usage: pep-hook.py $REPOS $REV
-# (standard post-commit args)
+# usage: python3 pep2rss.py .
 
 import datetime
 import glob
@@ -10,8 +9,48 @@ import re
 import sys
 import time
 import PyRSS2Gen as rssgen
+import docutils.frontend
+import docutils.nodes
+import docutils.parsers.rst
+import docutils.utils
 
 RSS_PATH = os.path.join(sys.argv[1], 'peps.rss')
+
+
+def remove_prefix(text: str, prefix: str) -> str:
+    try:
+        # Python 3.9+
+        return text.removeprefix(prefix)
+    except AttributeError:
+        if text.startswith(prefix):
+            return text[len(prefix):]
+        return text
+
+
+def parse_rst(text: str) -> docutils.nodes.document:
+    parser = docutils.parsers.rst.Parser()
+    components = (docutils.parsers.rst.Parser,)
+    settings = docutils.frontend.OptionParser(components=components).get_default_values()
+    document = docutils.utils.new_document('<rst-doc>', settings=settings)
+    parser.parse(text, document)
+    return document
+
+
+def pep_abstract(full_path: str) -> str:
+    """Return the first paragraph of the PEP abstract"""
+    abstract = None
+    with open(full_path, encoding="utf-8") as f:
+        text = f.read()
+        document = parse_rst(text)
+        nodes = list(document)
+        for node in nodes:
+            if "<title>Abstract</title>" in str(node):
+                for child in node:
+                    if child.tagname == "paragraph":
+                        abstract = child.astext()
+                        # Just fetch the first paragraph
+                        break
+    return abstract
 
 
 def firstline_startingwith(full_path, text):
@@ -59,11 +98,13 @@ for dt, full_path in peps_with_dt[:10]:
         pass
     title = firstline_startingwith(full_path, 'Title:')
     author = firstline_startingwith(full_path, 'Author:')
+    abstract = pep_abstract(full_path)
     url = 'https://www.python.org/dev/peps/pep-%0.4d/' % n
     item = rssgen.RSSItem(
         title='PEP %d: %s' % (n, title),
         link=url,
-        description='Author: %s' % author,
+        description=abstract,
+        author=author,
         guid=rssgen.Guid(url),
         pubDate=dt)
     items.append(item)
