0e38a534426a6324c2327a438e5501bb912d8308
Hugo van Kemenade <hugovk@users.noreply.github.com>
2020-10-23 13:04:22 +0300

    Flake8 fixes

--- pep2rss.py
+++ pep2rss.py
@@ -3,10 +3,16 @@
 # usage: pep-hook.py $REPOS $REV
 # (standard post-commit args)
 
-import os, glob, time, datetime, stat, re, sys
+import datetime
+import glob
+import os
+import re
+import sys
+import time
 import PyRSS2Gen as rssgen
 
 RSS_PATH = os.path.join(sys.argv[1], 'peps.rss')
+
 
 def firstline_startingwith(full_path, text):
     for line in open(full_path, encoding="utf-8"):
@@ -14,10 +20,13 @@ def firstline_startingwith(full_path, text):
             return line[len(text):].strip()
     return None
 
+
 # get list of peps with creation time
 # (from "Created:" string in pep .rst or .txt)
 peps = glob.glob('pep-*.txt')
 peps.extend(glob.glob('pep-*.rst'))
+
+
 def pep_creation_dt(full_path):
     created_str = firstline_startingwith(full_path, 'Created:')
     # bleh, I was hoping to avoid re but some PEPs editorialize
@@ -35,6 +44,8 @@ def pep_creation_dt(full_path):
     except ValueError:
         t = time.strptime(created_str, '%d-%B-%Y')
     return datetime.datetime(*t[:6])
+
+
 peps_with_dt = [(pep_creation_dt(full_path), full_path) for full_path in peps]
 # sort peps by date, newest first
 peps_with_dt.sort(reverse=True)
@@ -50,11 +61,11 @@ for dt, full_path in peps_with_dt[:10]:
     author = firstline_startingwith(full_path, 'Author:')
     url = 'https://www.python.org/dev/peps/pep-%0.4d/' % n
     item = rssgen.RSSItem(
-        title = 'PEP %d: %s' % (n, title),
-        link = url,
-        description = 'Author: %s' % author,
-        guid = rssgen.Guid(url),
-        pubDate = dt)
+        title='PEP %d: %s' % (n, title),
+        link=url,
+        description='Author: %s' % author,
+        guid=rssgen.Guid(url),
+        pubDate=dt)
     items.append(item)
 
 # the rss envelope
@@ -64,11 +75,11 @@ language features, and some meta-information like release
 procedure and schedules
 """.strip()
 rss = rssgen.RSS2(
-    title = 'Newest Python PEPs',
+    title='Newest Python PEPs',
     link = 'https://www.python.org/dev/peps/',
-    description = desc,
-    lastBuildDate = datetime.datetime.now(),
-    items = items)
+    description=desc,
+    lastBuildDate=datetime.datetime.now(),
+    items=items)
 
 with open(RSS_PATH, 'w', encoding="utf-8") as fp:
     fp.write(rss.to_xml(encoding="utf-8"))
