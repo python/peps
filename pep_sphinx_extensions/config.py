9368f991f0adbd078407ea0fb4747b06dbab8fae
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-04-20 03:34:32 +0100

    Add config for pep_processor

---
+++ pep_sphinx_extensions/config.py
@@ -0,0 +1,6 @@
+"""Miscellaneous configuration variables for the PEP Sphinx extensions."""
+
+pep_stem = "pep-{:0>4}"
+pep_url = f"{pep_stem}.html"
+pep_vcs_url = "https://github.com/python/peps/blob/master/"
+pep_commits_url = "https://github.com/python/peps/commits/master/"
