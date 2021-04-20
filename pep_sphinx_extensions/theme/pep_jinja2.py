6060f771286730bd1896a865871aeea86f3946c7
AA Turner <9087854+AA-Turner@users.noreply.github.com>
2021-04-20 23:07:13 +0100

    Add Sphinx Jinja2 custom template bridge

---
+++ pep_sphinx_extensions/theme/pep_jinja2.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import re
+from sphinx import jinja2glue
+from typing import TYPE_CHECKING
+
+from pep_sphinx_extensions.config import pep_stem
+
+if TYPE_CHECKING:
+    from sphinx.builders import Builder
+    from sphinx.theming import Theme
+
+pep_title_pat = re.compile(r"(PEP \d+).*")
+
+
+def _pep_id(pep_full_title: str) -> str:
+    matches = pep_title_pat.match(pep_full_title.strip().upper())
+    if not matches:
+        return ""
+    pep_num = matches.group(1).strip().removeprefix("PEP ")
+    return pep_stem.format(pep_num).replace("-", " ").upper()
+
+
+class PEPTemplateLoader(jinja2glue.BuiltinTemplateLoader):
+    def init(self, builder: Builder, theme: Theme = None, dirs: list[str] = None):
+        super().init(builder, theme, dirs)
+        self.environment.filters["pep_id"] = _pep_id
