e89b7fbc4c773728f5cc54ff98d065b0e18c1534
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2022-02-25 18:11:48 +0000

    Use Node.findall

--- pep_sphinx_extensions/pep_processor/html/pep_html_builder.py
+++ pep_sphinx_extensions/pep_processor/html/pep_html_builder.py
@@ -38,7 +38,7 @@ class FileBuilder(StandaloneHTMLBuilder):
         toc_tree = self.env.tocs[docname].deepcopy()
         if len(toc_tree[0]) > 1:
             toc_tree = toc_tree[0][1]  # don't include document title
-            for node in toc_tree.traverse(nodes.reference):
+            for node in toc_tree.findall(nodes.reference):
                 node["refuri"] = node["anchorname"] or '#'  # fix targets
             toc = self.render_partial(toc_tree)["fragment"]
         else:
