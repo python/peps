7b0c4721499db040c931f80d128a2b582c73044f
Philipp A <flying-sheep@web.de>
2022-04-25 12:20:12 +0200

    Make the button into a tri state one

--- pep_sphinx_extensions/pep_theme/static/colour_scheme.js
+++ pep_sphinx_extensions/pep_theme/static/colour_scheme.js
@@ -1,36 +1,72 @@
-const dark = document.getElementById("css-dark");
-const pygmentsNormal = document.getElementById("pyg");
-const pygmentsDark = document.getElementById("pyg-dark");
+const pygmentsLight = document.getElementById("pyg-light")
+const pygmentsDark = document.getElementById("pyg-dark")
 
 const makeLight = () => {
-    dark.disabled = pygmentsDark.disabled = true
-    dark.media = pygmentsNormal.media = pygmentsDark.media = ""
-    pygmentsNormal.disabled = false
-
+    document.documentElement.dataset.colour_scheme = "light"
+    pygmentsDark.disabled = true
+    pygmentsLight.media = pygmentsDark.media = ""
+    pygmentsLight.disabled = false
+    localStorage.setItem("colour_scheme", "light")
 }
 
 const makeDark = () => {
-    dark.disabled = pygmentsDark.disabled = false
-    dark.media = pygmentsNormal.media = pygmentsDark.media = ""
-    pygmentsNormal.disabled = true
+    document.documentElement.dataset.colour_scheme = "dark"
+    pygmentsDark.disabled = false
+    pygmentsLight.media = pygmentsDark.media = ""
+    pygmentsLight.disabled = true
+    localStorage.setItem("colour_scheme", "dark")
 }
 
+const makeAuto = () => {
+    document.documentElement.dataset.colour_scheme = "auto"
+    pygmentsLight.media = "(prefers-color-scheme: light)"
+    pygmentsDark.media = "(prefers-color-scheme: dark)"
+    pygmentsLight.disabled = false
+    pygmentsDark.disabled = false
+    localStorage.setItem("colour_scheme", "auto")
+}
 
-const toggleColourScheme = () => {
-    const colourScheme = localStorage.getItem("colour_scheme")
+const cycleColourScheme = () => {
+    const colourScheme = document.documentElement.dataset.colour_scheme
     const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches
 
-    if ((colourScheme === "dark") || (!colourScheme && prefersDark)) {
-        makeLight()
-        localStorage.setItem("colour_scheme", "light")
-    } else {
-        makeDark()
-        localStorage.setItem("colour_scheme", "dark")
+    if (prefersDark) { // auto (dark) → light → dark
+        if (colourScheme === "auto") {
+            makeLight()
+            setNextButton("dark")
+        } else if (colourScheme === "light") {
+            makeDark()
+            setNextButton("auto")
+        } else {
+            makeAuto()
+            setNextButton("light")
+        }
+    } else { // auto (light) → dark → light
+        if (colourScheme === "auto") {
+            makeDark()
+            setNextButton("light")
+        } else if (colourScheme === "dark") {
+            makeLight()
+            setNextButton("auto")
+        } else {
+            makeAuto()
+            setNextButton("dark")
+        }
     }
+}
+
+const setNextButton = (nextTheme) => {
+    const label = nextTheme === "auto" ? 'Adapt to system theme' : `Switch to ${nextTheme} mode`
+    const button = document.getElementById("colour-scheme-cycler")
+    button.setAttribute( "aria-label", label)
+    button.setAttribute( "title", label)
 }
 
 /* set colour scheme from local storage */
 document.addEventListener("DOMContentLoaded", () => {
-    if (localStorage.getItem("colour_scheme") === "light") makeLight()
-    if (localStorage.getItem("colour_scheme") === "dark") makeDark()
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        setNextButton("light")
+    } else {
+        setNextButton("dark")
+    }
 })
