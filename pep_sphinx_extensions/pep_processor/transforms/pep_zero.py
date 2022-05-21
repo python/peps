799ed754168baab44c5d31ce3f19a557ca35062b
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2022-05-21 21:14:01 +0100

    Remove special PEP 0 post-processing

--- pep_sphinx_extensions/pep_processor/transforms/pep_zero.py
+++ pep_sphinx_extensions/pep_processor/transforms/pep_zero.py
@@ -18,18 +18,7 @@ class PEPZero(transforms.Transform):
 
 
 class PEPZeroSpecial(nodes.SparseNodeVisitor):
-    """Perform the special processing needed by PEP 0.
-
-    - Mask email addresses.
-    - Link PEP numbers and PEP titles in the table to the PEPs themselves.
-
-    """
-
-    def __init__(self, document: nodes.document):
-        super().__init__(document)
-        self.pep_table: int = 0
-        self.entry: int = 0
-        self.ref: str | None = None
+    """Mask email addresses in PEP 0."""
 
     def unknown_visit(self, node: nodes.Node) -> None:
         """No processing for undefined node types."""
@@ -39,49 +28,6 @@ class PEPZeroSpecial(nodes.SparseNodeVisitor):
     def visit_reference(node: nodes.reference) -> None:
         """Mask email addresses if present."""
         node.replace_self(_mask_email(node))
-
-    @staticmethod
-    def visit_field_list(node: nodes.field_list) -> None:
-        """Skip PEP headers."""
-        if "rfc2822" in node["classes"]:
-            raise nodes.SkipNode
-
-    def visit_tgroup(self, node: nodes.tgroup) -> None:
-        """Set column counter and PEP table marker."""
-        self.pep_table = node["cols"] == 4
-        self.entry = 0  # reset column number
-
-    def visit_colspec(self, node: nodes.colspec) -> None:
-        self.entry += 1
-        if self.pep_table and self.entry == 2:
-            node["classes"].append("num")
-
-    def visit_row(self, _node: nodes.row) -> None:
-        self.entry = 0  # reset column number
-        self.ref = None  # Reset PEP URL
-
-    def visit_entry(self, node: nodes.entry) -> None:
-        self.entry += 1
-        if not self.pep_table:
-            return
-        if self.entry == 2 and len(node) == 1:
-            node["classes"].append("num")
-            # if this is the PEP number column, replace the number with a link to the PEP
-            para = node[0]
-            if isinstance(para, nodes.paragraph) and len(para) == 1:
-                pep_str = para.astext()
-                try:
-                    pep_num = int(pep_str)
-                except ValueError:
-                    return
-                self.ref = self.document.settings.pep_url.format(pep_num)
-                para[0] = nodes.reference("", pep_str, refuri=self.ref)
-        elif self.entry == 3 and len(node) == 1 and self.ref:
-            # If this is the PEP title column, add a link to the PEP
-            para = node[0]
-            if isinstance(para, nodes.paragraph) and len(para) == 1:
-                pep_title = para.astext()
-                para[0] = nodes.reference("", pep_title, refuri=self.ref)
 
 
 def _mask_email(ref: nodes.reference) -> nodes.reference:
