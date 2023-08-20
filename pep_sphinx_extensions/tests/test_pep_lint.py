ce9d603f7324d328ad6b66b023e00b00a4ef4d6e
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2023-08-20 18:54:16 +0100

    Test check_direct_links

--- pep_sphinx_extensions/tests/test_pep_lint.py
+++ pep_sphinx_extensions/tests/test_pep_lint.py
@@ -58,6 +58,38 @@ def test_header_pattern_no_match(test_input):
     assert pep_lint.HEADER_PATTERN.match(test_input) is None
 
 
+@pytest.mark.parametrize(
+    "line",
+    [
+        "http://www.python.org/dev/peps/pep-0000/",
+        "https://www.python.org/dev/peps/pep-0000/",
+        "http://peps.python.org/pep-0000/",
+        "https://peps.python.org/pep-0000/",
+    ],
+)
+def test_check_direct_links_pep(line: str):
+    warnings = [
+        warning for (_, warning) in pep_lint.check_direct_links(1, line)
+    ]
+    assert warnings == ["Use the :pep:`NNN` role to refer to PEPs"], warnings
+
+
+@pytest.mark.parametrize(
+    "line",
+    [
+        "http://www.rfc-editor.org/rfc/rfc2324",
+        "https://www.rfc-editor.org/rfc/rfc2324",
+        "http://datatracker.ietf.org/doc/html/rfc2324",
+        "https://datatracker.ietf.org/doc/html/rfc2324",
+    ],
+)
+def test_check_direct_links_rfc(line: str):
+    warnings = [
+        warning for (_, warning) in pep_lint.check_direct_links(1, line)
+    ]
+    assert warnings == ["Use the :rfc:`NNN` role to refer to RFCs"], warnings
+
+
 def test_validate_required_headers():
     found_headers = dict.fromkeys(
         ("PEP", "Title", "Author", "Status", "Type", "Created")
