f205724b0eb99c4f89aef6aab955c14af766feee
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2023-09-05 04:11:23 +0100

    Consolidate paths

--- pep_sphinx_extensions/tests/conftest.py
+++ pep_sphinx_extensions/tests/conftest.py
@@ -2,10 +2,11 @@ import importlib.util
 import sys
 from pathlib import Path
 
-PEP_ROOT = Path(__file__, "..", "..", "..").resolve()
+_ROOT_PATH = Path(__file__, "..", "..", "..").resolve()
+PEP_ROOT = _ROOT_PATH
 
 # Import "check-peps.py" as "check_peps"
-CHECK_PEPS_PATH = Path(__file__).resolve().parents[2] / "check-peps.py"
+CHECK_PEPS_PATH = _ROOT_PATH / "check-peps.py"
 spec = importlib.util.spec_from_file_location("check_peps", CHECK_PEPS_PATH)
 sys.modules["check_peps"] = check_peps = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(check_peps)
