05a953f6d433adbdaa61317c2e078cd7b70c3eb6
Philipp A <flying-sheep@web.de>
2023-10-26 11:30:44 +0200

    Discard changes to check-peps.py

--- check-peps.py
+++ check-peps.py
@@ -416,12 +416,10 @@ def _validate_post_history(line_num: int, body: str) -> MessageIterator:
         for post in line.removesuffix(",").strip().split(", "):
             if not post.startswith("`") and not post.endswith(">`__"):
                 yield from _date(offset, post, "Post-History")
-            elif post.startswith("`") and post.endswith(">`__"):
+            else:
                 post_date, post_url = post[1:-4].split(" <")
                 yield from _date(offset, post_date, "Post-History")
                 yield from _thread(offset, post_url, "Post-History")
-            else:
-                yield offset, f"post line must be a date or both start with “`” and end with “>`__”"
 
 
 def _validate_resolution(line_num: int, line: str) -> MessageIterator:
