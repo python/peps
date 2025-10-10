364ae9c8fb2e3b868ce90b72149fdd89380fefa6
Petr Viktorin <encukou@gmail.com>
2025-10-10 15:21:54 +0200

    Put token use in the main example

--- peps/pep-0793/examplemodule.c
+++ peps/pep-0793/examplemodule.c
@@ -1,6 +1,10 @@
 /*
-Example module with C-level module-global state, and a simple function to
-update and query it.
+Example module with C-level module-global state, and
+
+- a simple function that updates and queries the state
+- a class wihose repr() queries the same module state (for an example of
+  PyType_GetModuleByToken)
+
 Once compiled and renamed to not include a version tag (for example
 examplemodule.so on Linux), this will run succesfully on both regular
 and free-threaded builds.
@@ -13,6 +17,13 @@ print(examplemodule.increment_value())  # 1
 print(examplemodule.increment_value())  # 2
 print(examplemodule.increment_value())  # 3
 
+
+class Subclass(examplemodule.ExampleType):
+    pass
+
+instance = Subclass()
+print(instance)  # <Subclass object; module value = 3>
+
 */
 
 // Avoid CPython-version-specific ABI (inline functions & macros):
@@ -23,6 +34,10 @@ print(examplemodule.increment_value())  # 3
 typedef struct {
     int value;
 } examplemodule_state;
+
+static PyModuleDef_Slot examplemodule_slots[];
+
+// increment_value function
 
 static PyObject *
 increment_value(PyObject *module, PyObject *_ignored)
@@ -37,10 +52,51 @@ static PyMethodDef examplemodule_methods[] = {
     {NULL}
 };
 
+// ExampleType
+
+static PyObject *
+exampletype_repr(PyObject *self)
+{
+    /* To get module state, we cannot use PyModule_GetState(Py_TYPE(self)),
+     * since Py_TYPE(self) might be a subclass defined in an unrelated module.
+     * So, use PyType_GetModuleByToken.
+     */
+    PyObject *module = PyType_GetModuleByToken(
+        Py_TYPE(self), examplemodule_slots);
+    if (!module) {
+        return NULL;
+    }
+    examplemodule_state *state = PyModule_GetState(module);
+    if (!state) {
+        return NULL;
+    }
+    return PyUnicode_FromFormat("<%T object; module value = %d>",
+                                self, state->value);
+}
+
+static PyType_Spec exampletype_spec = {
+    .name = "examplemodule.ExampleType",
+    .flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+    .slots = (PyType_Slot[]) {
+        {Py_tp_repr, exampletype_repr},
+        {0},
+    },
+};
+
+// Module
+
 static int
 examplemodule_exec(PyObject *module) {
     examplemodule_state *state = PyModule_GetState(module);
     state->value = -1;
+    PyTypeObject *type = (PyTypeObject*)PyType_FromModuleAndSpec(
+        module, &exampletype_spec, NULL);
+    if (!type) {
+        return -1;
+    }
+    if (PyModule_AddType(module, type) < 0) {
+        return -1;
+    }
     return 0;
 }
 
@@ -56,7 +112,7 @@ static PyModuleDef_Slot examplemodule_slots[] = {
 };
 
 // Avoid "implicit declaration of function" warning:
-PyMODEXPORT_FUNC PyModExport_examplemodule(PyObject *);
+PyMODEXPORT_FUNC PyModExport_examplemodule(void);
 
 PyMODEXPORT_FUNC
 PyModExport_examplemodule(void)
